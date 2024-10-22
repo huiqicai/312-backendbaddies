cb613b5496cd9248a825be4958e856b7e176d187
rynnnnc <139408696+ryannnc@users.noreply.github.com>
2024-10-22 18:00:21 -0400

    fixed docker and layout for frontend

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template
 
-app = Flask(__name__, template_folder='frontend', static_folder='frontend/static')
+app = Flask(__name__, template_folder='Frontend', static_folder='Frontend/static')
 
 @app.route("/")
 def home():
