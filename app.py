b5e84220c93f4b6759660bde4fdea6ee4fb1d6e0
bonniem27 <bonniemai.contact@gmail.com>
2024-10-22 17:46:58 -0400

    added routes

--- app.py
+++ app.py
@@ -4,7 +4,19 @@ app = Flask(__name__, template_folder='frontend', static_folder='frontend/static
 
 @app.route("/")
 def home():
-    return render_template("bb.html")  
+    return render_template("bb.html")
+
+@app.route("/css")
+def css_path():
+    return render_template("style.css")
+
+@app.route("/image")
+def image_path():
+    return render_template("")
+
+@app.route("/js")
+def js_path():
+    return render_template("")
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=8080)  
